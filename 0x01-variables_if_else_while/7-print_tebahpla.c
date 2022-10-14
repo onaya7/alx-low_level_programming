875b7a2be8770770baf58da80f64a7ea4ef98484
samuel <samuelayano7@gmail.com>
2022-10-14 13:18:13 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+/**
+  * main - Program entry point
+  *
+  * Return: 0 on success. Error code otherwise
+  */
+int main(void)
+{
+	char z = 'z';
+
+	while (z >= 'a')
+	{
+		putchar(z);
+		z--;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
