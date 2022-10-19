d00e11ad1ddf9ade8ccd653753ea46ae16cde765
samuel <samuelayano7@gmail.com>
2022-10-19 07:13:09 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+  * _islower - a program thaat checks for lowercase character.
+  *
+  * Return: (0) on success .else error
+  */
+
+int _islower(int object)
+{
+	if (object >= 'a' && object <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
