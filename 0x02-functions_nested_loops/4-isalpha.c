aa9957b992d05622e06bea3af19ce7f44cded655
samuel <samuelayano7@gmail.com>
2022-10-19 08:54:46 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.c"
+
+/**
+  *_isalpha - program that checks for alphabetic characters
+  *
+  *@object: an alphabet
+  *
+  *Return: 1 if successful otherwise 0
+  */
+
+int _isalpha(int object)
+{
+	if ((object >= 'a' && object <= 'z') || (object >= 'A' && object <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
