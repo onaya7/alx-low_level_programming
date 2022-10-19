8cec46f08fa47978d73a01330d847b0a583cd645
samuel <samuelayano7@gmail.com>
2022-10-19 03:59:10 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+  * main - A program that prints _putchar followed by a new line.
+  *
+  *
+  * Return: (0) on success. Error code otherwise
+  */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
