86ed6c40a3dcb0c38ee4ab06fc0206c48227ca43
samuel <samuelayano7@gmail.com>
2022-10-19 04:53:23 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+  * print_alphabet - a program that prints the alphabet, in lowercase.
+  *
+  * Return: (0) on success .else error
+  */
+
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
