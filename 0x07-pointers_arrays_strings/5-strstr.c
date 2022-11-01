c203a90f0f11b07a13e0faab4166104e2dd811a6
samuel <samuelayano7@gmail.com>
2022-11-01 13:14:02 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-   *  _strstr - returns pointer to first char of matching substring
-    *  @haystack: string to find substring in
-     *  @needle: substring to find match of
-      *  Return: pointer to first char of matching substring
-       *
-        */
+  *  _strstr - returns pointer to first char of matching substring
+  *  @haystack: string to find substring in
+  *  @needle: substring to find match of
+  *  Return: pointer to first char of matching substring
+  *
+  */
 
 char *_strstr(char *haystack, char *needle)
 {
