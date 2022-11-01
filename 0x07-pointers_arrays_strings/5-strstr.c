12e84ad4c9761287b4b603baa784d4aeafbbcc34
samuel <samuelayano7@gmail.com>
2022-11-01 13:10:30 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,39 +1,26 @@
-nclude "main.h"
+#include "main.h"
 
 /**
-  * _strstr - Locates a substring.
-  * @haystack: The string to be searched.
-  * @needle: The substring to be located.
-  *
-  * Return: If the substring is located - a pointer to the beginning
-  * of the located substring.
-  * If the substring is not located - NULL.
-  */
+   *  _strstr - returns pointer to first char of matching substring
+    *  @haystack: string to find substring in
+     *  @needle: substring to find match of
+      *  Return: pointer to first char of matching substring
+       *
+        */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int index;
+	int k;
 
-	if (*needle == 0)
-		return (haystack);
-
-	while (*haystack)
+	while (*haystack != '\0')
 	{
-		index = 0;
 
-		if (haystack[index] == needle[index])
-		{
-			do {
-				if (needle[index + 1] == '\0')
-					return (haystack);
-
-				index++;
-
-			} while (haystack[index] == needle[index]);
-		}
-
-		haystack++;
+		k = 0;
+		while (*haystack == *needle && *haystack != '\0' && *needle != '\0')
+			haystack++, needle++, k++;
+		if (*needle == '\0')
+			return (haystack - k);
+		haystack -= (k - 1), needle -= k;
 	}
-
 	return ('\0');
 }
